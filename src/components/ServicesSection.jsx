0dad5b9d3065a1e3f804cce80eab6864ec8dd405
Devansh-Sehgal <devanshsehgal42@gmail.com>
2025-03-14 00:44:55 +0530

    Update ServicesSection.jsx

--- src/components/ServicesSection.jsx
+++ src/components/ServicesSection.jsx
@@ -1,8 +1,8 @@
-
 import { BarChart3, Cloud, Code, Palette, ShoppingBag, Truck } from 'lucide-react';
 import ServiceCard from './ServiceCard';
 
 const ServicesSection = () => {
+
   const services = [
     {
       icon: BarChart3,
@@ -37,28 +37,39 @@ const ServicesSection = () => {
   ];
 
   return (
-    <section id="services" className="py-20 bg-muted/50">
-      <div className="container mx-auto px-4 md:px-6">
-        <div className="text-center mb-16 max-w-3xl mx-auto">
-          <h2 className="text-3xl md:text-4xl font-bold mb-4">Our Services</h2>
+
+    <section id="services" className="py-4 bg-muted/50">
+
+      <div className='flex flex-col p-2 gap-2'>
+
+        <div className="text-center mb-4 max-w-3xl mx-auto ">
+          <h2 className="text-3xl md:text-4xl font-bold mb-2">Our Services</h2>
           <p className="text-lg text-muted-foreground">
             Comprehensive solutions designed to transform retail operations with cutting-edge technology and industry expertise.
           </p>
         </div>
-        
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 md:gap-8">
-          {services.map((service, index) => (
-            <div 
-              key={index} 
-              className="animate-fade-up" 
-              style={{ animationDelay: `${0.1 + index * 0.1}s` }}
-            >
-              <ServiceCard {...service} />
-            </div>
-          ))}
+
+
+        <div className="flex flex-col h-[420px] p-3 items-center md:justify-center w-full rounded-3xl gap-10 overflow-y- scrollbar-hide">
+
+          <div className="relative grid grid-cols-1 rounded-2xl md:grid-cols-2 gap-6 overflow-hidden overflow-y-scroll scrollbar-hide">
+
+            {services.map((service, index) => (
+              
+              <div
+                key={index}
+                className="animate-fade-up sticky top-0 rounded-2xl p-4"
+                style={{ animationDelay: `${0.1 + index * 0.1}s` }}
+              >
+                <ServiceCard {...service} index={index} />
+
+              </div>
+            ))}
+          </div>
         </div>
-      </div>
-    </section>
+      </div >
+
+    </section >
   );
 };
 
